afdd6d3a04d84bcdbcfaf7bc0e904a8e439b6203
Jarno Le Conté <jarno.leconte@me.com>
2025-07-23 23:55:47 +0200

    Prevent rendering black screen on iOS when loading video from a local blob url and not using the autoplay feature.

--- src/three/materials/screen.material.tsx
+++ src/three/materials/screen.material.tsx
@@ -4,9 +4,19 @@ import { useMainStore } from "~/store";
 export function ScreenMaterial({ videoSource }: { videoSource: string }) {
   const pageProgress = useMainStore((state) => state.pageProgress);
 
-  const texture = useVideoTexture(videoSource, { start: false, loop: false });
-  texture.flipY = false;
-  texture.image.currentTime = pageProgress * texture.image.duration;
+  // Create a video texture
+  const texture = useVideoTexture(videoSource, {
+    playsInline: true,
+    muted: true,
+    loop: false,
+    autoplay: true, // Required for iOS, otherwise it just renders a black screen
+    start: false, // this ensures the video doesn't start playing immediately
+  });
 
-  return <meshBasicMaterial map={texture} toneMapped={false} />;
+  texture.flipY = false;
+
+  // Scrubbing the video when scroll position changes
+  texture.image.currentTime = pageProgress * (texture.image.duration || 0);
+
+  return texture && <meshBasicMaterial map={texture} toneMapped={false} />;
 }
